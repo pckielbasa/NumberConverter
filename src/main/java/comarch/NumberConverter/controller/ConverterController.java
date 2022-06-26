bab672225f3e03fbf29d5b82f23b09d7e2a04386
pckielbasa <pckielbasa@gmail.com>
2022-06-26 15:04:44 +0200

    Added html convert page

--- src/main/java/comarch/NumberConverter/controller/ConverterController.java
+++ src/main/java/comarch/NumberConverter/controller/ConverterController.java
@@ -1,21 +1,39 @@
 package comarch.NumberConverter.controller;
 
-import comarch.NumberConverter.model.Converter;
 import comarch.NumberConverter.repository.ConverterRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/convert")
 @RequiredArgsConstructor
 public class ConverterController {
 
-    @Autowired
     private ConverterRepo converterRepo;
 
-    @GetMapping("/to")
+    @Autowired
+    public ConverterController(ConverterRepo converterRepo) {
+        this.converterRepo = converterRepo;
+    }
+
+    @RequestMapping("/home")
+    public ModelAndView home()
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello world";
+    }
+
+    @GetMapping("/convert")
     public String convertNumber(@RequestParam (value = "number") Long number,
                                    @RequestParam (value = "convertTo") String convertTo) {
         return converterRepo.convertNumber(number,convertTo);
